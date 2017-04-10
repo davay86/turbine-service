7b988a3bb50b6fca162f9854fbb76635acd8a041
James Fairbairn <jaybono30@hotmail.com>
2017-04-10 07:16:36 +0100

    Renamed Spring Boot Class

--- turbine-server-parent/turbine-server/src/main/resources/start-turbine-servers.bat
+++ turbine-server-parent/turbine-server/src/main/resources/start-turbine-servers.bat
@@ -1,4 +1,4 @@
 MODE 1000,1000
 
-start "turbine-server-1" java -jar @server.jar.name@.jar --server.port=1111 --spring.application.name=turbine-server-one
-start "turbine-server-2" java -jar @server.jar.name@.jar --server.port=1112 --spring.application.name=turbine-server-two
+start "turbine-server-1" java -jar @server.jar.name@.jar --server.port=1111 --spring.application.name=turbine-server
+REM start "turbine-server-2" java -jar @server.jar.name@.jar --server.port=1112 --spring.application.name=turbine-server-two
