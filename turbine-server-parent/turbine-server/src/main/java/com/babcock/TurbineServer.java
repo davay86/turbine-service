43b176480c859fe4b6f113a22e602db1a645577c
James Fairbairn <jaybono30@hotmail.com>
2017-04-10 16:41:53 +0100

    Renamed Spring Boot file

--- turbine-server-parent/turbine-server/src/main/java/com/babcock/TurbineServer.java
+++ turbine-server-parent/turbine-server/src/main/java/com/babcock/TurbineServer.java
@@ -3,12 +3,14 @@ package com.babcock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+//import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableDiscoveryClient
-@EnableTurbine
-@EnableHystrixDashboard
+@EnableTurbineStream
+//@EnableTurbine
+//@EnableHystrixDashboard
 @SpringBootApplication
 public class TurbineServer {
 	public static void main(String[] args) {
